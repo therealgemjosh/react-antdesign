8057ebd1f9aef6f4349e1968b47e0d97a77963d1
gems <therealgemjosh@gmail.com>
2020-10-29 20:04:24 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import React,{useState}  from 'react';
+import {Button,DatePicker,message,Alert} from 'antd';
 import './App.css';
+import {Footer} from './footer.js';
+
+
+
+
 
 function App() {
+const [date,setDate]= useState(null);
+	const handleChange = value=>{
+		
+		message.info(`Selected Date: ${value?value.format('YYYY-MM-DD'):'NONE'}`);
+		setDate(value);
+	}
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+	<div className="Header">
+	  <h2>Simple Mobile UI Design</h2>
+	  </div>
+
+	  <div className="Appcontent">
+		<div style={{width: "400px",margin:'100px, auto'}}>
+	  		<DatePicker onChange={handleChange} style={{margin:'5px 0px'}}/>
+	  <Alert message="Selected date:" description={date?date.format('YYYY-MM-DD'):'NONE'} style={{marginBottom:'5px',width:'90%'}}/>
+
+	  	</div>
+
+
+		  <Button type="primary"
+	  		onClick={()=> handleChange}
+	  >Submit</Button>
+	  </div>
+
+	  <Footer/>
     </div>
   );
 }
---
+++ src/footer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Footer=()=>(
+	<div className="Footer">
+		<p>copyright &copy; 2020 Antd.</p>
+	</div>
+)
